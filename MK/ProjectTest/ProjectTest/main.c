8f1021d3fda97d14f3d85c5840a7332859b38b8b
Temur2006 <salnikovtemur2006@gmail.com>
2025-07-01 14:24:36 +0300

    MK_added

---
+++ MK/ProjectTest/ProjectTest/main.c
@@ -0,0 +1,18 @@
+/*
+ * ProjectTest.c
+ *
+ * Created: 01.07.2025 14:23:27
+ * Author : romaT
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
